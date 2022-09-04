7bdf6c961ee30a8d1b2f84164d598f93c604c355
igal <igalfsg@gmail.com>
2022-09-04 14:48:26 -0400

    AAdded windows build

---
+++ BlazorApp1/buildWindows.ps1
@@ -0,0 +1,7 @@
+$certificateSubjectName = "CN=Coding Flamingo"
+New-SelfSignedCertificate -Type Custom -Subject $certificateSubjectName  -KeyUsage DigitalSignature -FriendlyName "Coding Flamingo Self Sign" -CertStoreLocation "Cert:\CurrentUser\My" -TextExtension @("2.5.29.37={text}1.3.6.1.5.5.7.3.3", "2.5.29.19={text}")
+
+$cert = (Get-ChildItem cert:\CurrentUser\My   | where-object { $_.Subject -like "$certificateSubjectName" }  | Select-Object -First 1).Thumbprint
+
+dotnet publish -f net6.0-windows10.0.19041 -c Release /p:PackageCertificateThumbprint=$cert /p:GenerateAppxPackageOnBuild=true
+/p:AppxPackageSigningEnabled=true
